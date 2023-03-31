a9fe5c8eaf25d868d6b42afd7af605b30dd29089
Hamza Mirza <hamza.tariq.mirza@gmail.com>
2023-03-31 06:20:03 +0500

    framework setup

---
+++ config/hooks.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Add the name of the hooks and attach all associated classes in it
+ *
+ */
+return [
+
+
+
+];
---
+++ config/menu.php
@@ -0,0 +1,15 @@
+<?php
+
+
+return [
+
+    "menu_title"=>"WP Whales Control",
+    "page_title"=>"WP Whales",
+    "parent_menu_title"=>"Control",
+    "slug"=>"wp-whales",
+    "capability"=>"manage_options",
+    "icon"=>null,
+    "position"=>10,
+    "base_view_file"=>"admin-menu-base"
+
+];
